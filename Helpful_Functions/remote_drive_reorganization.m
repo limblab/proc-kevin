5e405f48e9d9b06cbcb394d98298eb4ddf82f957
unknown <kb0dkin@northwestern.edu>
2020-09-17 10:35:26 -0500

    very beginning of script to reorganize the data folder of the remote drive

---
+++ Helpful_Functions/remote_drive_reorganization.m
@@ -0,0 +1,12 @@
+function status = remote_drive_reorganization(varargin)
+%% status = remote_drive_reorganization(varargin)
+%
+% Rearranges directories in the limblab/data directory to fit the
+% "monkey/date/*.xyz" data structure. 
+% 
+% optional inputs:
+% 
+% directory: a string of the directory to rearrange 
+% monkey: monkey's name
+%
+
