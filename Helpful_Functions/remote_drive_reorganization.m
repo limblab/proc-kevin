2b17ae0703a3fe3d73dd282a09149a4178b7ba56
Kevin Bodkin <kb0dkin@github.com>
2021-08-11 15:43:53 -0500

    rewrite of the server reorganization script to look through nev file headers and use system directory listing rather than matlab, since it seems to work a lot more quickly

--- Helpful_Functions/remote_drive_reorganization.m
+++ Helpful_Functions/remote_drive_reorganization.m
@@ -26,7 +26,7 @@ end
 
 % we're going to use .nev and .plx files as our base to figure out         %some .plx in Tiki, and probably other old monkeys
 % dates, then move all of the other related files.
-f_ext = {'.nev','.plx'};
+% f_ext = {'.nev','.plx'};
 %f_ext = {'.nev','.ns1','.ns2','.ns3','.ns4','.ns5','.ns6','.plx','.mat','.ccf','.png','.fig','.jpg', '.rhd'};
 
 
@@ -38,8 +38,10 @@ plxList = listFile(baseDir,'.plx');
 
 
 %% create necessary directories and move all files
-moved_files = {'Filename','name matches creation date'}; %do we want to fill out structures here?
-duplicate_files = {'Filename', 'name matches creation date'}; %still to be developed
+movedFileList = {};
+skippedFileList = {};
+scannedDirs = {};
+
 
 % first deal with all of the nevs
 for ii = 1:length(nevList) 
@@ -54,54 +56,46 @@ for ii = 1:length(nevList)
     end
     
     matchFileList = getMatchFiles(nevList{ii},baseDir,'.nev'); % get all of the files with the same name, different extensions
-%    matchFileList{end+1} = nevList{ii}; %this seems to make a repeat of the .nev
     
-    for jj = 1:length(matchFileList)
-        movefile(char(matchFileList(jj)), currTargetDir);  
+    for jj = 1:length(matchFileList) % get all of the matching files and move them over
+        tempFN = strsplit(matchFileList{ii},filesep);
+        tempDir = strjoin(tempFN(1:end-1),filesep);
+        tempFN = tempFN{end};
+        targetFN = [currentTargetDir,filesep,tempFN];
+        if ~exist(targetFN,'file')
+            movefile(matchFileList{ii},targetFN)
+            movedFileList{end+1} = {matchFileList{ii},targetFN};
+            scannedDirectories{end+1} = tempDir;
+        else
+            skippedFileList{end+1} = {tempFN, 'File Already Exists in target location'};
+        end
     end
     
-    moved_files = dir(currTargetDir);
-    moved_files = moved_files(3:end); %fix: just get the names
     
 end
 
 %% Clean up empty directories
 
-log_file = [baseDir,filesep,'log.xlsx'];
+% looks through all of the directories that had .nev files that were moved,
+% then looks inside of those directories to see what wasn't moved.
+%
+% Will also remove any empty directories that are in that list
 
-rem_dirs = {};
-untouch_file = {};
+remDirs = {};
+untouchFile = {};
 
-%scanned_dirs = unique({baseDir.folder}); %think this is attempting to get 
-                                          %all folders within the directory 
-                                          %specified, but this can't be dot
-                                          %indexed, at least on Mac
-                                         
-%try this instead:
-scanned_dirs = dir(fullfile(baseDir,'**'));         %recursively gets all folders, subfolders, and files from baseDir
-%scanned_dirs = scanned_dirs([scanned_dirs.isdir]);  %keeps only the folders...but causes problems, see below
-scanned_dirs = scanned_dirs(~ismember({scanned_dirs.name},{'.','..'})); %gets rid of current and parent directories
+scannedDirs = unique(scannedDirs); % to avoid repetition
 
-for ii = 1:numel(scanned_dirs)            %this will be a problem if it's all files inside
-%    sd_info = dir(scanned_dirs(ii));     %trying to get all the contents of each directory?
-%    sd_info = sd_info(3:end);            %and remove ., ..?
+for ii = 1:numel(scannedDirs)
 
-    %try this instead:
-    sd_info = dir(strcat(scanned_dirs(ii).folder,filesep,scanned_dirs(ii).name)); %since everything in 'name' is a folder
-    sd_info = sd_info(3:end);
-    
-    % i added this stuff but at this point sd_info is just one file
-%    if ismac
-%        sd_info = sd_info(4:end);  %Mac also has .DS_Store
-%    else
-%        sd_info = sdinfo(3:end);
-        
-    if isempty(sd_info)
-        rem_dirs{end+1} = scanned_dirs{ii}; %can we pre-allocate?
-        rmdir(scanned_dirs{ii})             %remove empty folder
+    sdContents = dir(scannedDirs{ii}); % what's inside of the scanned dir?
+    sdContents = sdContents(3:end); % removing '.' and '..'
+
+    if isempty(sdContents)
+        remDirs{end+1} = scannedDirs{ii}; %can we pre-allocate?
+        rmdir(scannedDirs{ii})             %remove empty folder
     else
-        sd_info = sd_info(~[sd_info.isdir]);
-        untouch_file(end+1:end+numel(sd_info)) = {sd_info.name};  %if there's something left in the original folder?
+        untouchFile{end+1:end+numel(sdContents)} = {sdContents};  %if there's something left in the original folder?
     end
     
     
@@ -110,30 +104,34 @@ end
 
 %% Spreadsheets with info about the session
 
-try
-    xlswrite(log_file,rem_dirs,'Removed Directories')
-catch
-    disp('No directories removed')
-end
-
-try
-    xlswrite(log_file,untouch_file,'Untouched Files')
-catch
-    disp('No files untouched')
-end
-
-try 
-    xlswrite(log_file, duplicate_files, 'Duplicate files') %causes a problem trying to write cell arrays
-catch
-    disp('No duplicate files detected')
-end
+save([targetDir,filesep,'fileReorganizationData'],'movedFileList','skippedFileList',...
+    'scannedDirs','remDirs','untouchFile');
 
 
-try
-    xlswrite(log_file,moved_files,'Moved Files')
-catch
-    disp('No files moved') %says no files moved, but that's not true
-end
+% try
+%     xlswrite(log_file,rem_dirs,'Removed Directories')
+% catch
+%     disp('No directories removed')
+% end
+% 
+% try
+%     xlswrite(log_file,untouch_file,'Untouched Files')
+% catch
+%     disp('No files untouched')
+% end
+% 
+% try 
+%     xlswrite(log_file, duplicate_files, 'Duplicate files') %causes a problem trying to write cell arrays
+% catch
+%     disp('No duplicate files detected')
+% end
+% 
+% 
+% try
+%     xlswrite(log_file,moved_files,'Moved Files')
+% catch
+%     disp('No files moved') %says no files moved, but that's not true
+% end
     
 end
 
@@ -150,22 +148,17 @@ function fileList = listFile(currDir,fileExt)
     if ispc
         [stat,fileList] = system(['dir /s /b ',currDir,filesep,'*',fileExt]);
     else
-%        [stat,fileList] = system(['ls -R ',currDir,filesep,'*',fileExt]);  %not looking recursively
-        %Try this:
-        fileList = dir(fullfile(currDir,'**')); %look recursively
-        fileList = (struct2cell(fileList))';    %easier format to work with
-        fileList(:,2:end) = [];                 %only need names
-        fileList = fileList(contains(fileList,fileExt));
+       [stat,fileList] = system(['ls -R ', currDir, '| grep \\', fileExt]);  %looking recursively
     end
     
-%   fileList = strsplit(fileList); % split by line breaks and tab delimiters
-%   fileList = fileList(~cellfun(@isempty,fileList)); % remove any empty entries
+  fileList = strsplit(fileList); % split by line breaks and tab delimiters
+  fileList = fileList(~cellfun(@isempty,fileList)); % remove any empty entries
 
     
 end
 
 
-% strips files to just get the "base" filename
+%% looks for filenames with the same "base" filename
 function matchFileList = getMatchFiles(filepath,baseDir,fileExt)
     % tack a period on the front of the extension if needed
     if ~contains(fileExt,'.')
