46fc2508508f8720a7ccabb1eaa5d819617e763d
Kevin Lanning Bodkin <klb807@fsm.northwestern.edu>
2023-08-29 13:54:11 -0500

    correcting count vs firing rate error, R2 issue, and updating replay method

--- MNReg_Decoding/Danzinger_Decoder_LimblabInitialWork.m
+++ MNReg_Decoding/Danzinger_Decoder_LimblabInitialWork.m
@@ -4,15 +4,13 @@
 % conversions
 
 monkey = 'Tot'; % monkey name
-task = 'WM'; % task
+task = 'WS'; % task
 array_name = 'left_M1';
 ran_by = 'XM_KLB'; % who recorded this?
-lab = 6; % upstairs
+lab = 1; % upstairs
 bin_width = .05; % 50 ms
 sorted = 0;
 requires_raw_emg = 0; % are we recording EMG here?
-
-delay = 0; % number of bins to delay by -- 2 bins w/ 50 ms bin == 100 ms
 
 params = struct(...
     'monkey_name',monkey,...
@@ -37,14 +35,6 @@ save_dir = 'D:\Kevin\';
 xds = raw_to_xds(base_dir, file_name, map_dir, map_name, params);
 base_name = strsplit(file_name,'.nev');
 
-% add delay as desired
-temp_p = [xds.curs_p(delay+1:end,:); zeros(delay,2)];
-temp_v = [xds.curs_v(delay+1:end,:); zeros(delay,2)];
-temp_a = [xds.curs_a(delay+1:end,:); zeros(delay,2)];
-xds.curs_p = temp_p; % overwrite
-xds.curs_v = temp_v; % overwrite
-xds.curs_a = temp_a; % overwrite
-xds.cursor_delay = delay;
 
 % useful for later -- neurons and sample numbers
 num_samples = size(xds.spike_counts,1); % number of samples
@@ -58,6 +48,11 @@ disp('XDS created and saved')
 %% use existing xds
 [file_name, base_dir] = uigetfile('*_xds.mat');
 load([base_dir,filesep,file_name]);
+
+% useful for later -- neurons and sample numbers
+num_samples = size(xds.spike_counts,1); % number of samples
+num_neurons = size(xds.spike_counts,2); % number of neurons
+
 
 %% parse the xds data
 %
@@ -96,15 +91,8 @@ offline_xds = raw_to_xds(base_dir, file_name, map_dir, map_name, params);
 %% offline testing -- existing xds
 
 
-%%
-% add delay as desired
-temp_p = [offline_xds.curs_p(delay+1:end,:); zeros(delay,2)];
-temp_v = [offline_xds.curs_v(delay+1:end,:); zeros(delay,2)];
-temp_a = [offline_xds.curs_a(delay+1:end,:); zeros(delay,2)];
-offline_xds.curs_p = temp_p; % overwrite
-offline_xds.curs_v = temp_v; % overwrite
-offline_xds.curs_a = temp_a; % overwrite
-offline_xds.cursor_delay = delay;
+%% plotting cursor velocities and positions
+
 
 % % Continuous control
 % store predictions
@@ -220,16 +208,18 @@ title('Predicted cursor, offline data, trial aligned, start trial at 0')
 %% compare with W Filter
 
 % create the historical data lags
-num_taps = 10; % start with 10 lags
-lagged_spikes = zeros(num_samples,num_taps*num_neurons + 1);
-lagged_spikes(:,num_taps*num_neurons + 1) = 1; % term to capture the mean
+num_lags = 10; % start with 10 lags
+lagged_spikes = zeros(num_samples,num_lags*num_neurons);
+% lagged_spikes = zeros(num_samples,num_lags*num_neurons + 1);
+% lagged_spikes(:,num_lags*num_neurons + 1) = 1; % term to capture the mean
 % fill lagged spike matrix
-for replay_idx = 0:(num_taps-1)
-    neur_offset = replay_idx*num_neurons;
-    lagged_spikes(1:num_samples-replay_idx,(1:num_neurons)+neur_offset) = xds.spike_counts((replay_idx+1):num_samples,:);
+for lag_idx = 0:(num_lags-1)
+    neur_offset = lag_idx*num_neurons;
+    lagged_spikes(1+lag_idx:num_samples,(1:num_neurons)+neur_offset) = xds.spike_counts(1:num_samples-lag_idx,:);
 end
 
-filter_W = lagged_spikes\xds.curs_v; % could do the normal equation or something, but this is quicker ;)
+% basic ridge regression -- saw little change
+filter_W = inv(lagged_spikes'*lagged_spikes + .1*eye(size(lagged_spikes,2)))*(lagged_spikes'*xds.curs_v); % could do the normal equation or something, but this is quicker ;)
 
 % calculate quality of predictions -- both velocity and cursor
 pred_vel = lagged_spikes * filter_W; 
@@ -239,13 +229,46 @@ pred_curs = cumsum(pred_vel)*bin_width;
 % nlm = fitnlm(xds.curs_v(:,1),pred_vel(:,1),'y ~ (b0+ b1*x + b3*x^3)', [0,1,1]);
 
 % calculate varience explained
-vaf_vel = 1 - sum((pred_vel-xds.curs_v).^2./var(xds.curs_v,1),1);
-vaf_curs = 1 - sum((pred_curs-xds.curs_p).^2./var(xds.curs_p,1),1);
+vaf_vel = 1 - mean((pred_vel-xds.curs_v).^2,1)./var(xds.curs_v,1);
+vaf_curs = 1 - mean((pred_curs-xds.curs_p).^2,1)./var(xds.curs_p,1);
 
 
+% plot them
+figure
+ax(1) = subplot(2,1,1);
+plot(xds.curs_v(:,1))
+hold on
+plot(pred_vel(:,1))
+title('X Velocities')
+legend('true','predicted')
+ax(2) = subplot(2,1,2);
+plot(xds.curs_v(:,2))
+hold on
+plot(pred_vel(:,2))
+title('Y Velocities')
+legend('true','predicted')
+linkaxes(ax,'x')
 
 
-%% Run online
+figure
+subplot(2,1,1)
+plot(xds.curs_p(:,1))
+hold on
+plot(pred_curs(:,1))
+title('X Position')
+legend('true','predicted')
+subplot(2,1,2)
+plot(xds.curs_p(:,2))
+hold on
+plot(pred_curs(:,2))
+title('Y Position')
+legend('true','predicted')
+
+
+%% Run through xpc
+
+% online? if not we'll replay an old file
+online_flag = 1;
 
 % wiener filter or multinomial?
 MN_flag = 1;
@@ -255,28 +278,51 @@ xpc_ip = '192.168.0.1'; % for sending the UDP packets of the X and Y
 xpc_port = 24999; % command port
 
 if verLessThan('matlab','9.9')
-    u = udp(xpc_ip, xpc_port); % create opject
-    set(u,'ByteOrder','littleEndian');
-    fopen(u); % and open it
+    xpc = udp(xpc_ip, xpc_port); % create opject
+    set(xpc,'ByteOrder','littleEndian');
+    fopen(xpc); % and open it
 else
-    u = udpport('LocalHost','192.168.0.3','LocalPort',24999);
-    set(u,'ByteOrder','little-endian');
+    xpc = udpport('LocalHost','192.168.0.3','LocalPort',24999);
+    set(xpc,'ByteOrder','little-endian');
+end
+
+% get the xds file if we're replaying stuff
+if online_flag == false
+    [xds_name, base_dir] = uigetfile('*_xds.mat');
+    load([base_dir,filesep,xds_name]);
 end
 
 
-% initialize the cerebus
+% get the recording directory if not already known
 if ~exist('rec_dir','var')
     rec_dir = uigetdir('C:','Recording directory'); % where are we recording?
 end
-rec_name = strcat(rec_dir,filesep,datestr(now,'YYYYmmdd_hhMM'), '_', monkey, '_', task, '_');
-cbmex('open'); % connect to cerebus
-cbmex('fileconfig',rec_name,'',0); % initialize file storage
 
-%store predictions
-predfile = [rec_dir, filesep, datestr(now, 'YYYYmmdd_hhMM'), '_', monkey, '_', task, '_MNParsPredictions.txt'];
-fid_pred = fopen(predfile, 'w+');
-spikefile = [rec_dir, filesep, datestr(now, 'YYYYmmdd_hhMM'), '_', monkey, '_', task, '_Spikes.txt'];
-fid_spike = fopen(spikefile,'w+');
+% initialize the cerebus -- only for online
+if online_flag == true
+    rec_name = strcat(rec_dir,filesep,datestr(now,'YYYYmmdd_hhMM'), '_', monkey, '_', task, '_');
+    cbmex('open'); % connect to cerebus
+
+    cbmex('fileconfig',rec_name,'',0); % initialize file storage
+end
+
+
+%store predictions -- different names online vs replay
+if mn_flag == true
+    bci_name = 'MultiNomial';
+else
+    bci_name = 'Wiener';
+end
+
+if online_flag == true
+    predfile = [rec_dir, filesep,  monkey, '_', datestr(now, 'YYYYmmdd_hhMM'), '_', task, '_', bci_name, 'Predictions.txt'];
+    fid_pred = fopen(predfile, 'w+');
+    spikefile = [rec_dir, filesep,  monkey, '_', datestr(now, 'YYYYmmdd_hhMM'), '_',task, '_Spikes.txt'];
+    fid_spike = fopen(spikefile,'w+');
+else
+    predfile = [rec_dir, filesep,  xds_file, '_replay_', datestr(now, 'YYYYmmdd_hhMM'), '_', bci_name, '.txt'];
+    fid_pred = fopen(predfile, 'w+');
+end
 
 
 % setup the output and inputs
@@ -284,7 +330,7 @@ fid_spike = fopen(spikefile,'w+');
 if MN_flag == true
     firing_buffer = zeros(1,num_neurons);
 else
-    firing_buffer = zeros(1,num_neurons*num_taps + 1);
+    firing_buffer = zeros(1,num_neurons*num_lags);
 end
 curs = [0, 0, 0, 0]; % curs pos, curs vel 
 
@@ -293,13 +339,23 @@ h = msgbox('Press ''ok''  to stop recording');
 
 % for now we'll just set it up as a loop. Can switch to timer() later if we
 % want
-cbmex('fileconfig',rec_name,'Danzinger decoder online',1)
-cbmex('trialconfig',1)
+if online_flag == true
+    cbmex('fileconfig',rec_name,[bci_name,' online recordings'],1)
+    cbmex('trialconfig',1)
+end
+
+% start the loop timer -- 
 tic_buf = tic;
 elapse_tic = tic;
 
 % Clear the buffer to make sure the initial outputs are reasonable
-[ts_cell_array, tBuffer,~] = cbmex('trialdata',1);
+if online_flag == true
+    [ts_cell_array, tBuffer,~] = cbmex('trialdata',1);
+end
+
+% counts going through the recording
+replay_idx = 1;
+
 while ishandle(h)
     
     % if it's within 100 us of the loop time
@@ -307,16 +363,21 @@ while ishandle(h)
     if toc_buf > bin_width
         tic_buf = tic; % set a new loop time
         
-        [ts_cell_array, tBuffer,~] = cbmex('trialdata',1);
-        
-        % pull in the number of spikes that have happened per channel in
-        % the last bin period
-        % works for both the wiener and the multinomial, since we'll shift
-        % the wiener at the end of each loop
-        for replay_idx = 1:num_neurons
-            firing_buffer(replay_idx) = length(ts_cell_array{replay_idx,2})/bin_width; % counts in hz
+        if online_flag == true
+            [ts_cell_array, tBuffer,~] = cbmex('trialdata',1);
+            
+            % pull in the number of spikes that have happened per channel in
+            % the last bin period
+            % works for both the wiener and the multinomial, since we'll shift
+            % the wiener at the end of each loop
+            for replay_idx = 1:num_neurons
+                firing_buffer(replay_idx) = length(ts_cell_array{replay_idx,2}); % counts in hz
+            end
+
+        else % just pull things out if it's a replay session
+            firing_buffer(1:num_neurons) = xds.spike_counts(replay_idx,:);
         end
-        
+
         % Run it through the decoder
         if MN_flag == true
             curs = MultinomialSelection(firing_buffer', dpars_naive, curs);
@@ -324,29 +385,47 @@ while ishandle(h)
             curs(3:4) = firing_buffer*filter_W; % vel = rates * W == 1x2 output
             curs(1:2) = curs(1:2) + curs(3:4).*bin_width; % velocity to position
             % need to shift the lags over
-            firing_buffer(num_neurons+1:num_taps*num_neurons) = firing_buffer(1:(num_taps-1)*num_neurons);
+            firing_buffer(num_neurons+1:num_lags*num_neurons) = firing_buffer(1:(num_lags-1)*num_neurons);
         end
-            
         
-        
+        % cursor corrections -- to make the system usable!
+        % limit at the edge of the screen
+        curs(1:2) = max(-12,max(12,curs(1:2)));
+
+
         % parse to send to the XPC
         if verLessThan('matlab','9.9')
-            fwrite(u,[0,0,curs(1),curs(2)],'single') % send to xpc
+            fwrite(xpc,[0,0,curs(1),curs(2)],'single') % send to xpc
         else
-            write(u,[0,0,curs(1),curs(2)],'single',xpc_ip, xpc_port);
+            write(xpc,[0,0,curs(1),curs(2)],'single',xpc_ip, xpc_port);
         end
         
         loop_time = toc(elapse_tic);
+        
         % store the data
         % cursor
         fprintf(fid_pred,'%.04f\t',loop_time);
         fprintf(fid_pred,'%.02f\t',curs); 
         fprintf(fid_pred,'\n');
+        
         % spikes
-        fprintf(fid_spike,'%.04f\t',loop_time);
-        fprintf(fid_spike, '%d\t', firing_buffer);
-        fprintf(fid_spike,'\n');
-        
+        if online_flag == true
+            fprintf(fid_spike,'%.04f\t',loop_time);
+            fprintf(fid_spike, '%d\t', firing_buffer);
+            fprintf(fid_spike,'\n');
+        end
+
+
+        % next replay step. Stop if we've reached the end!
+        if online_flag == false
+            replay_idx = replay_idx + 1;
+            if replay_idx > size(xds.curs_p,1)
+                close(h)
+            end
+        end
+                
+
+
         % wait for (loosely) the loop time
         pause(bin_width-.001 - toc(tic_buf))
         
@@ -354,86 +433,19 @@ while ishandle(h)
 
 end
 
+if online_flag == true
+    % close everything down
+    cbmex('fileconfig',rec_name,'',0)
+    cbmex('trialconfig',0)
+    cbmex('close')
+end
 
-% close everything down
-cbmex('fileconfig',rec_name,'',0)
-cbmex('trialconfig',0)
-cbmex('close')
+% close the udp socket
+clear xpc
 
-% if verLessThan(
-% fclose(u);
-clear u
-
+% close open files
 fclose(fid_pred);
-fclose(fid_spike);
-
-%% replay a recorded file
-% allows us to watch it replay on the xpc
-
-% wiener filter or multinomial?
-MN_flag = 0;
-
-% xpc settings -- for communication
-xpc_ip = '192.168.0.1'; % for sending the UDP packets of the X and Y
-xpc_port = 24999; % command port
-
-if verLessThan('matlab','9.9')
-    u = udp(xpc_ip, xpc_port); % create opject
-    set(u,'ByteOrder','littleEndian');
-    fopen(u); % and open it
-else
-    u = udpport('LocalHost','192.168.0.3','LocalPort',24999);
-    set(u,'ByteOrder','little-endian');
-%     set(u,'LocalPort',24999);
+if online_flag == true
+    fclose(fid_spike); 
 end
 
-if MN_flag == true
-    firing_buffer = zeros(1,num_neurons);
-else
-    firing_buffer = zeros(1,num_neurons*num_taps + 1);
-end
-curs = [0, 0, 0, 0]; % curs pos, curs vel 
-
-
-replay_idx = 0;
-while ishandle(h) && replay_idx < size(xds.spike_counts,1)
-    
-    % if it's within 100 us of the loop time
-    toc_buf = toc(tic_buf);
-    if toc_buf > bin_width
-        tic_buf = tic; % set a new loop time
-        
-        firing_buffer(1:num_neurons) = xds.spike_counts(replay_idx,:);
-        
-        % Run it through the decoder
-        if MN_flag == true
-            curs = MultinomialSelection(firing_buffer', dpars_naive, curs);
-        else
-            curs(3:4) = firing_buffer*filter_W; % vel = rates * W == 1x2 output
-            curs(1:2) = curs(1:2) + curs(3:4).*bin_width; % velocity to position
-            % need to shift the lags over
-            firing_buffer(num_neurons+1:num_taps*num_neurons) = firing_buffer(1:(num_taps-1)*num_neurons);
-        end
-            
-        
-        
-        % parse to send to the XPC
-        if verLessThan('matlab','9.9')
-            fwrite(u,[0,0,curs(1),curs(2)],'single') % send to xpc
-        else
-            write(u,[0,0,curs(1),curs(2)],'single',xpc_ip, xpc_port);
-        end
-        
-        loop_time = toc(elapse_tic);
-        % store the data
-        % cursor
-        
-        % wait for (loosely) the loop time
-        pause(bin_width-.001 - toc(tic_buf))
-        
-        % increment the counter
-        replay_idx = replay_idx+1;
-        
-    end
-
-end
